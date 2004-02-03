e8087426d942f36645ea45b4ba4130e79ef88c70
Domizio Demichelis <domizio@cpan.org>
2004-02-03 20:08:15 +0000

    Import of DOMIZIO/CGI-Builder-0.01 from CPAN.
    gitpan-cpan-distribution: CGI-Builder
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DOMIZIO/CGI-Builder-0.01.tar.gz
    gitpan-cpan-author:       DOMIZIO
    gitpan-cpan-maturity:     released

---
+++ lib/CGI/Builder/HTMLTemplate.pm
@@ -0,0 +1,12 @@
+package CGI::Builder::HTMLTemplate ;
+$VERSION = 0.01 ;
+
+=head1 DESCRIPTION
+
+Right now this distribution is just a placeholder. the real distribution will be uploaded ASAP.
+
+=head1 AUTHOR and COPYRIGHT
+
+© 2004 by Domizio Demichelis.
+
+All Rights Reserved. This module is free software. It may be used, redistributed and/or modified under the same terms as perl itself.
