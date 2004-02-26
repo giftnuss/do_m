6e460ca636b9f762e8ab920597a5c3be2e98bcaa
Domizio Demichelis <domizio@cpan.org>
2004-02-26 12:29:57 +0000

    Import of DOMIZIO/IO-Util-1.1 from CPAN.
    gitpan-cpan-distribution: IO-Util
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         DOMIZIO/IO-Util-1.1.tar.gz
    gitpan-cpan-author:       DOMIZIO
    gitpan-cpan-maturity:     released

--- lib/IO/Util.pm
+++ lib/IO/Util.pm
@@ -7,27 +7,42 @@ $VERSION = 1.01 ;
 ; our @ISA = 'Exporter'
 ; our @EXPORT_OK = qw(capture)
 
+; my $output
+
 ; sub capture (&;*)
    { my $code = shift()
+   ; $output = ''
    ; local(*H) = shift() || select()
-   ; my $output = ''
-   ; tie *H
-       , __PACKAGE__
-       , \$output
-   ; &{$code}()
-   ; untie *H
+   ; if (my $to = tied *H)
+      { my $tc = ref $to
+      ; bless $to, __PACKAGE__
+      ; &{$code}()
+      ; bless $to, $tc
+      }
+     else
+      { tie(*H , __PACKAGE__)
+      ; &{$code}()
+      ; untie *H
+      }
    ; \$output
    }
-
+                  
 ; sub TIEHANDLE
    { bless \@_, shift()
    }
 
 ; sub PRINT
-   { my $s = shift
-   ; ${$$s[0]} .= join( ($,||''), map{ defined($_) ? $_ : '' } @_ )
+   { shift
+   ; $output .= join( ($,||''), map{ defined($_) ? $_ : '' } @_)
+   ; $output .= $\||''
    }
 
+; sub PRINTF
+   { shift
+   ; my $fmt = shift
+   ; $output .= sprintf($fmt, @_)
+   }
+   
 ; 1
 
 __END__
@@ -36,7 +51,7 @@ __END__
 
 IO::Util - Captures the output sent to a file handler
 
-=head1 VERSION 1.01
+=head1 VERSION 1.1
 
 
 =head1 SYNOPSIS
@@ -46,16 +61,20 @@ IO::Util - Captures the output sent to a file handler
     # captures the selected file handler
     $output_ref = capture { some_printing_code } ;
     
-    # captures FILEHANDLER
-    $output_ref = capture { some_printing_code } FILEHEANDLER ;
-    
     sub some_printing_code {
         print 'something'
+    }
+    
+    # captures FILEHANDLER
+    $output_ref = capture { some_special_printing_code } FILEHEANDLER ;
+    
+    sub some_special_printing_code {
+        print FILEHANDLER 'something'
     }
 
 =head1 DESCRIPTION
 
-This is a micro-weight module that exports just the C<capture> function. It executes the code inside the first argument block, and captures the output it sends to the selected file handler (or to a specific file handler). It "hijacks" all the C<write> and C<print> statements addressed to the captured filehandler, returning the scalar reference of the output. Sort of "print to scalar" function.
+This is a micro-weight module that exports just the C<capture> function. It executes the code inside the first argument block, and captures the output it sends to the selected file handler (or to a specific file handler). It "hijacks" all the C<print> and C<printf> statements addressed to the captured filehandler, returning the scalar reference of the output. Sort of "print to scalar" function.
 
 =head1 INSTALLATION
 
@@ -81,6 +100,8 @@ From the directory where this file is located, type:
 =head2 capture { code } [ FILEHANDLER ]
 
 The C<capture> function espects a I<code> block as the first argument and an optional I<FILEHANDLER> as the second argument. If I<FILEHANDLER> is omitted the selected file handler will be used by default (usually C<STDOUT>). The function returns the reference to the captured output.
+
+B<Note>: This function ties the I<FILEHANDLER> to IO::Util class and unties it after the execution of the I<code>. If I<FILEHANDLER> is already tied to some other class, it just re-bless the tied object to IO::Util class and re-bless again the tied object to its original class after the execution of the I<code>, thus preserving the original I<FILEHANDLER> configuration.
 
 =head1 SUPPORT and FEEDBACK
 
--- t/01_all.t
+++ t/01_all.t
@@ -1,6 +1,6 @@
 #!perl -w
 ; use strict
-; use Test::More tests => 2
+; use Test::More tests => 4
 
 
 ; use IO::Util qw(capture)
@@ -20,3 +20,40 @@
 ; is ( $$out
      , 'cd'
      )
+
+; select STDOUT
+
+
+
+; tie *STDOUT, 'My::test_tie'
+
+; $out = capture { print_something('e'); print_something('f')}
+; ok (  ($$out eq 'ef')
+     && (ref(tied *STDOUT) eq 'My::test_tie')
+     )
+; untie *STDOUT
+
+; $, = '*'
+; $\ = '#'
+
+; $out = capture { print 'X', 'Y'
+                 ; printf '%2$d %1$d', 12, 34
+                 ; print_something('Z');
+                 }
+; is( $$out
+    , 'X*Y#34 12Z#'
+    )
+
+; package My::test_tie
+
+; my $output = ''
+
+; sub TIEHANDLE
+   { bless \@_, shift()
+   }
+
+; sub PRINT
+   { my $s = shift
+   }
+
+
