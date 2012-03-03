111683e83ab395a55e4d3e2b96872548558f0c27
Sebastian B. Knapp <rock@ccls-online.de>
2012-03-03 02:15:33 +0100

    error with newer perls fixed

--- ootools/lib/Class/Util.pm
+++ ootools/lib/Class/Util.pm
@@ -26,8 +26,9 @@ use strict ;
    ; my $r = eval "require $_;"
    ; if ($@)
       { (my $c = $_.'.pm') =~ s|\b::\b|/|g
+      ; no strict 'refs'
       ; croak $@ if $@ !~ /^Can't locate $c in \@INC/
-                    || not defined %{$_.'::'}
+                    || not grep { /[^:][^:]\z/ } keys %{$_.'::'}
       }
    ; $r
    }
